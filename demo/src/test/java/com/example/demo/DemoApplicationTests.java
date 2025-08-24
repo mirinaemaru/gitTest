65d279b831bf682fc7e3e6284618b55a8767873c
박창수 <mirinae.maru@gmail.com>
2025-08-24 17:52:56 +0900

    mirinae2에서 수정

---
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * This class represents the unit tests for the DemoApplication class. It is annotated with @SpringBootTest
+ * to configure the application context for testing. It contains a single test method contextLoads() which
+ * is used to ensure that the application context loads successfully.
+ */
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    // 컨텍스트로드 테스트
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/Calculator.java
@@ -0,0 +1,7 @@
+class Calculator {
+    Calculator() {};
+
+    public int calc(int n) {
+        return n + 1;
+    }
+}
